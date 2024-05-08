0e8d44d91a5f210b0576f12a0281ef14d4f539f2
Danimartinez25 <danimartinez1325@gmail.com>
2024-05-07 21:01:10 -0300

    replace with mock-product

--- src/use-cases/products/add-product.spec.ts
+++ src/use-cases/products/add-product.spec.ts
@@ -1,25 +1,32 @@
-import { describe, expect, test } from "vitest";
+import { beforeAll, describe, expect, test } from "vitest";
 import { addProduct } from "./add-product";
-import { mockProductService } from "../../mocks/product-service-mock";
+import {
+  MockedProductService,
+  mockProductService,
+} from "../../mocks/product-service-mock";
 import { mockCryptoService } from "../../mocks/crypto-service-mock";
 import { isUUID } from "../../utils/validations/vaildation-utils";
+import { mockProduct } from "../../utils/test/mock-product";
 
 describe("Create product", () => {
+  let productService: MockedProductService;
+  const crypto = mockCryptoService();
+  beforeAll(async () => {
+    productService = mockProductService([mockProduct(), mockProduct()]);
+  });
   test("with the correct data create the product", async () => {
-    const productService = mockProductService();
-    const cryptoService = mockCryptoService();
     await addProduct(
-      { products: productService, crypto: cryptoService },
+      { products: productService, crypto },
       {
         name: "product",
         description: "Description Product",
         starts: 3,
       }
     );
-    expect(productService.products.length).toBe(1);
-    expect(isUUID(productService.products[0].id)).toBe(true);
-    expect(productService.products[0].name).toBe("product");
-    expect(productService.products[0].description).toBe("Description Product");
-    expect(productService.products[0].starts).toBe(3);
+    expect(productService.products.length).toBe(3);
+    expect(isUUID(productService.products[2].id)).toBe(true);
+    expect(productService.products[2].name).toBe("product");
+    expect(productService.products[2].description).toBe("Description Product");
+    expect(productService.products[2].starts).toBe(3);
   });
 });
