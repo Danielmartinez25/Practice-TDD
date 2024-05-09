b7b27beb82cb410a88551684fb2a3e845877aa1a
Danimartinez25 <danimartinez1325@gmail.com>
2024-05-09 02:33:02 -0300

    product service

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -4,4 +4,6 @@ export interface ProductService {
   save(product: Product): Promise<void>;
   getAll(): Promise<Product[]>;
   delete(id: string): Promise<void>;
+  edit(product: Product): Promise<void>;
+  findById(id: string): Promise<Product | null>;
 }
