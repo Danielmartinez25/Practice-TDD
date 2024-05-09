40759521a9cefe33e32229f59f9cf1d601857b6d
Danimartinez25 <danimartinez1325@gmail.com>
2024-05-09 02:30:38 -0300

    create mock function edit and findById

--- src/mocks/product-service-mock.ts
+++ src/mocks/product-service-mock.ts
@@ -12,16 +12,26 @@ export function mockProductService(
   return {
     products: [...products],
     async save(product: Product) {
-      ms(100);
+      await ms(100);
       this.products.push(product);
     },
     async getAll() {
-      ms(100);
+      await ms(100);
       return this.products;
     },
     async delete(id: string) {
-      ms(100);
+      await ms(100);
       this.products = this.products.filter((product) => product.id === id);
+    },
+    async edit(product: Product) {
+      await ms(100);
+      const productIndex = this.products.findIndex((p) => p.id === product.id);
+      this.products.splice(productIndex, 1, product);
+    },
+    async findById(id: string) {
+      await ms(100);
+      const productFound = this.products.find((product) => product.id === id);
+      return productFound ? productFound : null;
     },
   };
 }
